dc031c1444c6c9e785648b66aee9e14b627c0cc1
Dustin Thomas <stdio@cptlobster.dev>
2025-02-19 20:56:57 -0600

    GPLv3

--- src/main/scala/dev/cptlobster/cta_tracker/models/Route.scala
+++ src/main/scala/dev/cptlobster/cta_tracker/models/Route.scala
@@ -1,5 +1,22 @@
+/* Copyright (C) 2025  Dustin Thomas <io@cptlobster.dev>
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.  If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
 package dev.cptlobster.cta_tracker.models
 
+/**
+ * Enum representation of CTA routes.
+ * @param s CTA representation of line names
+ */
 enum Route(s: String):
   case RED    extends Route("red")
   case BLUE   extends Route("blue")
@@ -10,4 +27,4 @@ enum Route(s: String):
   case PINK   extends Route("pink")
   case YELLOW extends Route("y")
 
-  def toString: String = s
+  override def toString: String = s
